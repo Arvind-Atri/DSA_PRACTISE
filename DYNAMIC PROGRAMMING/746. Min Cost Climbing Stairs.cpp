a83a6894baa23f748df1349c8a66c714b57af71a
Arvind-Atri <148221394+Arvind-Atri@users.noreply.github.com>
2026-09-10 08:58:40 +0530

    Update 746. Min Cost Climbing Stairs.cpp

--- DYNAMIC PROGRAMMING/746. Min Cost Climbing Stairs.cpp
+++ DYNAMIC PROGRAMMING/746. Min Cost Climbing Stairs.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
-    int solve(vector<int>& cost, int n, vector<int>& dp) {
-        if (n == 0)
-            return cost[0];
-        if (n == 1)
-            return cost[1];
-        if (dp[n] != -1)
-            return dp[n];
+    // int solve(vector<int>&dp,vector<int>&cost,int n){
+    //     if(n==0 || n==1){
+    //         return cost[n];
+    //     }
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=min(solve(dp,cost,n-1),solve(dp,cost,n-2))+cost[n];
+    // }
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     int n=cost.size();
+    //     // vector<int>dp(n+1,-1);
+    //     // return min(solve(dp,cost,n-1),solve(dp,cost,n-2));
+    //     if(n==0||n==1) return cost[n];
+    //     vector<int>dp(n+1);
+    //     dp[0]=cost[0];
+    //     dp[1]=cost[1];
+    //     for(int i=2;i<n;i++){
+    //         dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+    //     }
+    //     return min(dp[n-1],dp[n-2]);
 
-        dp[n] = min(solve(cost, n - 1, dp), solve(cost, n - 2, dp)) + cost[n];
-        return dp[n];
-    }
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1, -1);
-        int ans = min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
-        return ans;
-    }
-};
-
-
-
-
-
-
-// Tabulatin
-
-int minCostClimbingStairs(vector<int> &cost)
-       	{
-       	    int n = cost.size();
-       	    vector<int> dp(n, -1);
-       	    dp[0] = cost[0];
-       	    dp[1] = cost[1];
-       	    for (int i = 2; i < n; i++)
-       	        dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
-       	    return min(dp[n - 1], dp[n - 2]);
-       	}
-
-
-// Space Optimised
-
- int solve2(vector<int>&cost,int n){
-        
-
+    // }
+     int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        // vector<int>dp(n+1,-1);
+        // return min(solve(dp,cost,n-1),solve(dp,cost,n-2));
+        if(n==0||n==1) return cost[n];
+        // vector<int>dp(n+1);
         int prev2=cost[0];
         int prev1=cost[1];
-
         for(int i=2;i<n;i++){
-            int curr=cost[i]+min(prev2,prev1);
+            int curr=min(prev1,prev2)+cost[i];
             prev2=prev1;
             prev1=curr;
         }
         return min(prev1,prev2);
-       
+
     }
+};
