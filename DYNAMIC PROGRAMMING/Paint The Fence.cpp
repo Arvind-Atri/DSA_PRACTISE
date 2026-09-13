088b6cc37184256f5ab1e90daa48c63a826e7fc8
Arvind-Atri <148221394+Arvind-Atri@users.noreply.github.com>
2026-09-13 11:25:12 +0530

    Update Paint The Fence.cpp

--- DYNAMIC PROGRAMMING/Paint The Fence.cpp
+++ DYNAMIC PROGRAMMING/Paint The Fence.cpp
@@ -1,8 +1,15 @@
-#include <bits/stdc++.h> 
-const int mod = 1e9 + 7;
+
 
 https://www.youtube.com/watch?v=O3Rn-Kjr0p0
 
+#include <bits/stdc++.h> 
+#define mod 1000000007
+int solve(int n ,int k){
+    if(n==1) return k;
+    if(n==2) return (k*k);
+
+    return (k-1)*(solve(n-1,k)%mod+solve(n-2,k)%mod);
+}
 long long fun(int n, int k, vector<long long>& dp) {
         if (n == 1) return k % mod;
         if (n == 2) return (k * k) % mod;
@@ -17,9 +24,22 @@ long long fun(int n, int k, vector<long long>& dp) {
         dp[n] = result;
         return result;
 }
+int solveTab(int n ,int k){
+    vector< long long int>dp(n+1);
 
+    long long int mod=1e9 + 7;
+
+    dp[0]=0;
+    dp[1]=k%mod;
+    dp[2]=(k*k)%mod;
+
+    for(int i=3;i<=n;i++){
+        dp[i]=(k-1)*(dp[i-1]+dp[i-2])%mod;
+    }
+    return dp[n];
+}
 int numberOfWays(int n, int k) {
-    // Write your code here.
-    vector<long long> dp(n + 1, -1);
-    return fun(n, k, dp);
+    vector<long long int>dp(n+1,-1);
+    // return solve(n,k);
+    return fun(n,k,dp);
 }
